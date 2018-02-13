2152622ac546dbbebd3245b7d62350ad042f7ea9
Sukhman Kaur <Sukhman@sukhmans-macbook-pro.local>
2018-02-12 19:29:16 -0500

    Initial Commit

---
+++ golist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  golist
+//
+//  Created by Sukhman Kaur on 2/12/18.
+//  Copyright © 2018 Jaskaran D. Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
